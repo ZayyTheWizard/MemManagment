c8819d1bd1530ec172e359d9c9539f22f382d04e
JustEzek <88030885+JustEzek@users.noreply.github.com>
2024-08-04 03:06:08 -0400

    Create next_fit.py

---
+++ src/MemAlgorithms/next_fit.py
@@ -0,0 +1,11 @@
+def nextFit(arr, memory_blocks):
+    allocation = [-1] * len(arr)
+    j = 0
+    for i in range(len(arr)):
+        while j < len(memory_blocks):
+            if memory_blocks[j] >= arr[i]:
+                allocation[i] = j
+                memory_blocks[j] -= arr[i]
+                break
+        j = (j + 1) % len(memory_blocks)
+return allocation 
